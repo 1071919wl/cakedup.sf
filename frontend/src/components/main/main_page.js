e1cac8a6a2a3390c9606e2f903a8661fac277c4a
William Leung <w.leung.93@gmail.com>
2021-02-12 23:13:58 -0800

    main page edit

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -14,47 +14,17 @@ class MainPage extends React.Component {
                     <img alt="" src={tempBG} className='main-bg-img'/>
                 </div> */}
                 
-                {/* <div className='title_container'>
-                    <h1>- BASQUE CHEESECAKES -</h1>
-                    <h1>Cakedup.Sf</h1>
-                </div>
-
-                <div className='about_gurl_container'>
-
-
-                    <div className='gurl_container'>
-                        <img alt="" src={gurl} className='main-gurl-img'/>
-                    </div>
-                    
-                    <div className='about_gurl_info'>
-                        <div>
-                            <h1>Alice Hu</h1>
-                        </div>
-                        <div>
-                            <p>lorem ipsum dolor sit amet, consectet</p>
-                        </div>
-                        <div>
-                            <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.​ This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide.</p>
-                        </div>
-                        <div>
-                            <p>Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. Make your company stand out and show your visitors who you are.</p>
-                        </div>
-                    </div>
-                </div> */}
                 <div className='title_container'>
                     <h1 className='cake_type'>- BASQUE CHEESECAKES -</h1>
                     <h1 className='brand-name'>Cakedup.Sf</h1>
                 </div>
 
-                <div className ='testing' >
+                <div className ='about_gurl_container' >
 
-                    <div className="menu">
-                        <div className='gurl_container'>
-                            <img alt="" src={gurl} className='main-gurl-img'/>
-                        </div>
+
+                    <div className='gurl_container'>
+                        <img alt="" src={gurl} className='main-gurl-img'/>
                     </div>
-
-                    
 
                     <div className="about_gurl_info">
                         <div className="about_gurl_name"> 
@@ -71,20 +41,12 @@ class MainPage extends React.Component {
                         </div>
                     </div>
 
-                    <div className="right">
+                    {/* <div className="about">
                         <h2>About</h2>
                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-                    </div>
+                    </div> */}
+
                 </div>
-
-
-
-
-
-
-
-
-
 
 
                 <footer className='footer_container'>
